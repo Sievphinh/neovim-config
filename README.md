67ff9c38a9b5af22886eae28ec9df5285a2dfe57
sievphinh <cheathsievphygn@gmail.com>
2022-04-09 02:00:27 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+![image](https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Neovim-logo.svg/2560px-Neovim-logo.svg.png)
---
+++ lua/plugins/init.lua
@@ -0,0 +1,10 @@
+return require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
+  use {'rose-pine/neovim', config = "vim.cmd('colorscheme rose-pine')"}
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = ":TSUpdate",
+    event = "BufWinEnter",
+    config = "require('treesitter-config')"
+  }
+end)
